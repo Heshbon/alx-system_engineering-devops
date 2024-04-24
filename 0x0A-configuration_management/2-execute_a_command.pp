67b72955fec8ad191a8e8b118cf6c94f6c644cf6
Heshbon <cityalight.hesbon@gmail.com>
2024-04-24 03:09:13 -0700

    2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -4,5 +4,6 @@ exec { 'killing a process using pkill':
   command  => 'pkill -9 killmenow',
   path     => '/usr/bin:/bin',
   onlyif   => 'pgrep killmenow',
-  logoutput => true,
+  provider => shell,
+
 }
