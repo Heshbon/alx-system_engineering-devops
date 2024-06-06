f30c659866219318583046f09a936f38362dc7ec
Heshbon <cityalight.hesbon@gmail.com>
2024-06-06 14:05:13 -0700

    1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""The function to print hot posts on a given Reddit subreddit."""
+
+
+def top_ten(subreddit):
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    import requests
+
+    sub_details = requests.get(
+        "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit),
+        headers={"User-Agent": "My-User-Agent"},
+        allow_redirects=False
+    )
+    if sub_details.status_code >= 300:
+        print('None')
+    else:
+        [print(child.get("data").get("title"))
+         for child in sub_details.json().get("data").get("children")]
