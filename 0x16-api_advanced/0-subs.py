16545a4ba3b3b4d2c90a65805b9963d445428109
Heshbon <cityalight.hesbon@gmail.com>
2024-06-06 14:04:52 -0700

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""The function to query subscribers on a given Reddit subreddit."""
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers for a given subreddit"""
+    import requests
+
+    sub_details = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "My-User-Agent"},
+        allow_redirects=False
+    )
+    if sub_details.status_code >= 300:
+        return 0
+
+    return sub_details.json().get("data").get("subscribers")
